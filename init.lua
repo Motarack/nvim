e5f57ca8cbcb5356a4ce5abe417d7d867464b15a
motarack <mutasem_97@hotmail.com>
2025-08-06 15:37:29 +0300

    add some lsp features

--- init.lua
+++ init.lua
@@ -1,6 +1,20 @@
 require("plugins")
 require("set")
 
+vim.lsp.config('*', {
+	on_attach = function(client, bufnr)
+		vim.lsp.inlay_hint.enable()
+
+		vim.cmd[[set completeopt+=menuone,noselect,popup,fuzzy]]
+		local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
+        client.server_capabilities.completionProvider.triggerCharacters = chars
+		vim.lsp.completion.enable(true, client.id, bufnr, {
+			autotrigger = true,
+		})
+	end
+})
+
 vim.lsp.enable('lua_ls')
 vim.lsp.enable('pyrefly')
 vim.lsp.enable('gdscript')
+
