4b75e8d8adcfd4f1009dda88f5e2e5056562f4c4
motarack <mutasem_97@hotmail.com>
2026-04-22 11:15:13 +0300

    ignore uid files for godot in telescopt

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,12 @@
+require('telescope').setup({
+	defaults={
+		file_ignore_patterns={
+			"%.gd.uid",
+			"%.gdshader.uid",
+			"%.gdshaderinc.uid",
+		},
+	},
+})
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
