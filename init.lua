00156099f83e569bb479b793968fb9e4318c4aae
motarack <mutasem_97@hotmail.com>
2026-07-17 15:08:15 +0400

    remove spider plugin, adds more friction than removes it

--- init.lua
+++ init.lua
@@ -118,8 +118,3 @@ vim.keymap.set('n', '<leader>b', ':G blame<cr>')
 vim.pack.add({'https://github.com/windwp/nvim-autopairs'})
 require("nvim-autopairs").setup {}
 
-vim.pack.add({'https://github.com/chrisgrieser/nvim-spider'})
-vim.keymap.set({ "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>")
-vim.keymap.set({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>")
-vim.keymap.set({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>")
-vim.keymap.set({ "n", "o", "x" }, "ge", "<cmd>lua require('spider').motion('ge')<CR>")
